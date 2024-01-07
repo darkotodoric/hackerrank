e5fcfa6e85c071c39ebcf0456d973d0bfb2f950b
Darko Todoric <darko.todoric013@gmail.com>
2024-01-07 22:02:47 +0100

    data-structures

---
+++ data-structures/sock-merchant.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @param int $n
+ * @param array $ar
+ *
+ * @return int
+ */
+function sockMerchant(int $n, array $ar): int
+{
+    $pairs = [];
+    $total = 0;
+
+    foreach ($ar as $pair) {
+        if (isset($pairs[$pair])) {
+            $total++;
+
+            unset($pairs[$pair]);
+            continue;
+        }
+
+        $pairs[$pair] = 1;
+    }
+
+    return $total;
+}
