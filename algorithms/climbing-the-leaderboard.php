b8a9fe0598a6d1819439512acfa66ffd71f230d6
Darko Todoric <darko.todoric013@gmail.com>
2024-01-09 11:42:24 +0100

    algo

---
+++ algorithms/climbing-the-leaderboard.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @param array $ranked
+ * @param array $player
+ *
+ * @return array
+ */
+function climbingLeaderboard(array $ranked, array $player): array
+{
+    $uniqueRanked = array_reverse(array_values(array_unique($ranked)));
+    $ranks = [];
+    $lastRankPos = 0;
+
+    foreach ($player as $score) {
+        while ($lastRankPos < count($uniqueRanked) && $score >= $uniqueRanked[$lastRankPos]) {
+            $lastRankPos++;
+        }
+
+        $ranks[] = count($uniqueRanked) - $lastRankPos + 1;
+    }
+
+    return $ranks;
+}
---
+++ algorithms/picking-numbers.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @param array $a
+ *
+ * @return int
+ */
+function pickingNumbers(array $a): int
+{
+    sort($a);
+    $maxLength = 0;
+    $total = count($a);
+
+    for ($i = 0; $i < $total; $i++) {
+        $count = 1;
+
+        for ($j = $i + 1; $j < $total; $j++) {
+            if (abs($a[$j] - $a[$i]) <= 1) {
+                $count++;
+            } else {
+                break;
+            }
+        }
+
+        if ($count > $maxLength) {
+            $maxLength = $count;
+        }
+    }
+
+    return $maxLength;
+}
