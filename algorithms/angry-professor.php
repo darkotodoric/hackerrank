873746c3f88202d4c085853a9699b785e957aba5
Darko Todoric <darko.todoric013@gmail.com>
2024-01-09 13:34:02 +0100

    algo

---
+++ algorithms/angry-professor.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @param int $k
+ * @param array $a
+ *
+ * @return string
+ */
+function angryProfessor(int $k, array $a): string
+{
+    $late = 0;
+    foreach ($a as $studentArrival) {
+        if ($studentArrival <= 0) {
+            continue;
+        }
+
+        $late++;
+    }
+
+    if ((count($a) - $late) >= $k) {
+        return 'NO';
+    }
+
+    return 'YES';
+}
---
+++ algorithms/utopian-tree.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @param int $n
+ *
+ * @return int
+ */
+function utopianTree(int $n): int
+{
+    $isSpring = true;
+    $height = 1;
+
+    for ($x = 0; $x < $n; $x++) {
+        if ($isSpring) {
+            $height = $height * 2;
+            $isSpring = false;
+        } else {
+            $height++;
+            $isSpring = true;
+        }
+    }
+
+    return $height;
+}
